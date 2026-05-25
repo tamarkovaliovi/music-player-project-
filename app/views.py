82e3c26a8c48038db2b19e6c336f74b8c3bf3f4d
tamarkovaliovi <20222013305@std.nisantasi.edu.tr>
2026-05-25 09:38:44 +0300

    Order songs by title and tidy imports/settings

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Song
 
+
 def index(request):
-    songs = Song.objects.all() 
+    songs = Song.objects.all().order_by('title')
     return render(request, 'index.html', {'songs': songs})
